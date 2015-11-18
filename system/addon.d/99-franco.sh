51745672164d8765820f11d507890744b99d6f8f
FrancoGag <francog92@gmail.com>
2015-11-18 14:54:53 -0300

    Updated addon.d script

--- system/addon.d/99-franco.sh
+++ system/addon.d/99-franco.sh
@@ -1,7 +1,8 @@
 #!/sbin/sh
-# 
-# /system/addon.d/70-gapps.sh
 #
+# Backkup all files from Mako Tools
+#
+
 . /tmp/backuptool.functions
 
 list_files() {
@@ -39,14 +40,14 @@ EOF
 case "$1" in
   backup)
     list_files | while read FILE DUMMY; do
-      backup_file $S/$FILE
+      backup_file $S/"$FILE"
     done
   ;;
   restore)
     list_files | while read FILE REPLACEMENT; do
       R=""
       [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
-      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
     done
   ;;
   pre-backup)
