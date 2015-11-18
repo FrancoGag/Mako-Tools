a00a3a533efa357a72e5b36179e89da96b13bdb6
FrancoGag <francog92@gmail.com>
2015-11-18 19:40:14 -0300

    Fixed addon.d backup script

--- system/addon.d/99-franco.sh
+++ system/addon.d/99-franco.sh
@@ -2,39 +2,39 @@
 #
 # Backkup all files from Mako Tools
 #
-
 . /tmp/backuptool.functions
 
 list_files() {
 cat <<EOF
 priv-app/com.asksven.betterbatterystats_xdaedition.apk
-system/app/Toggle2G/Toggle2G.apk
-system/etc/hosts
-system/etc/init.d/01kernel
-system/etc/init.d/02sound
-system/media/bootanimation.zip
-system/vendor/egl/eglsubAndroid.so
-system/vendor/egl/libEGL_adreno.so
-system/vendor/egl/libGLESv1_CM_adreno.so
-system/vendor/egl/libGLESv2_adreno.so
-system/vendor/egl/libq3dtools_adreno.so
-system/vendor/lib/libadreno_utils.so
-system/vendor/lib/libbccQTI.so
-system/vendor/lib/libC2D2.so
-system/vendor/lib/libc2d30-a3xx.so
-system/vendor/lib/libCB.so
-system/vendor/lib/libgsl.so
-system/vendor/lib/libllvm-qcom.so
-system/vendor/lib/librs_adreno.so
-system/vendor/lib/librs_adreno_sha1.so
-system/vendor/lib/libRSDriver_adreno.so
-system/vendor/lib/libsc-a3xx.so
-system/xbin/fixsdcard
-system/xbin/fstrimall
-system/xbin/maintenance
-system/xbin/mako-tools
-system/xbin/sqlite3
-system/xbin/vacuum
+addon.d/99-franco.sh
+app/Toggle2G/Toggle2G.apk
+etc/hosts
+etc/init.d/01kernel
+etc/init.d/02sound
+media/bootanimation.zip
+vendor/egl/eglsubAndroid.so
+vendor/egl/libEGL_adreno.so
+vendor/egl/libGLESv1_CM_adreno.so
+vendor/egl/libGLESv2_adreno.so
+vendor/egl/libq3dtools_adreno.so
+vendor/lib/libadreno_utils.so
+vendor/lib/libbccQTI.so
+vendor/lib/libC2D2.so
+vendor/lib/libc2d30-a3xx.so
+vendor/lib/libCB.so
+vendor/lib/libgsl.so
+vendor/lib/libllvm-qcom.so
+vendor/lib/librs_adreno.so
+vendor/lib/librs_adreno_sha1.so
+vendor/lib/libRSDriver_adreno.so
+vendor/lib/libsc-a3xx.so
+xbin/fixsdcard
+xbin/fstrimall
+xbin/maintenance
+xbin/mako-tools
+xbin/sqlite3
+xbin/vacuum
 EOF
 }
 case "$1" in
