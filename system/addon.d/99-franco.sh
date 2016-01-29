476a99b5b8d2d3f4d0888191d63166be9739a974
FrancoGag <francog92@gmail.com>
2016-01-29 18:59:30 -0300

    Mako Tools 6.4.0

--- system/addon.d/99-franco.sh
+++ system/addon.d/99-franco.sh
@@ -12,12 +12,12 @@ app/Toggle2G/Toggle2G.apk
 etc/hosts
 etc/init.d/01kernel
 etc/init.d/02sound
+fonts/NotoColorEmoji.ttf
 media/bootanimation.zip
 xbin/fixsdcard
 xbin/fstrimall
 xbin/maintenance
 xbin/makotools
-xbin/pingg
 xbin/pingw
 xbin/sqlite3
 xbin/vacuum
@@ -50,9 +50,7 @@ case "$1" in
     sed -i '/ro.config.vc_music_vol_steps/c\ro.config.vc_music_vol_steps=15' /system/build.prop
     rm -rf /system/app/Stk/
     rm -rf /system/app/Calculator/
-	rm -rf /system/app/ExactCalculator/
+    rm -rf /system/app/ExactCalculator/
     rm -rf /system/app/LockClock/
-    rm -rf /system/priv-app/HotwordEnrollment/
-    rm -rf /system/priv-app/Velvet/
   ;;
 esac
